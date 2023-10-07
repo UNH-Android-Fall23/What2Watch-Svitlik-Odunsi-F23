af7013b90672000759fc86dcef57b394936e2e9b
sarah svitlik <svitliksarah@gmail.com>
2023-10-06 21:13:31 -0400

    Initial Commit
    Got things setup and updated the gitignore

---
+++ app/src/main/java/com/example/what2watch_svitlik_odunsi_f23/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.what2watch_svitlik_odunsi_f23.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
---
+++ app/src/main/java/com/example/what2watch_svitlik_odunsi_f23/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.what2watch_svitlik_odunsi_f23.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
