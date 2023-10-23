ca1abc9b891e1441059c4929a0fcc7a2cce04a11
sarah svitlik <svitliksarah@gmail.com>
2023-10-23 12:41:06 -0400

    Started working on the swipe view, but I need to finish later.
    I left off in the ViewPagerAdapter Kotlin file

---
+++ app/src/main/java/com/example/what2watch_svitlik_odunsi_f23/ui/ViewPagerAdapter.kt
@@ -0,0 +1,31 @@
+package com.example.what2watch_svitlik_odunsi_f23.ui
+
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.filament.View
+
+// TODO: finish the swipe view, only started this but I need to finish 
+/* 
+class ViewPagerAdapter (private var title: List<String>, private var details: List<String>, private var images:List<Int>): RecyclerView.Adapter<ViewPagerAdapter.Pager2ViewHolder>() {
+
+    inner class Pager2ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val itemGenre: TextView = itemView.findViewById(R.id.text_Genre)
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ViewPagerAdapter.Pager2ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewPagerAdapter.Pager2ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
+*/
