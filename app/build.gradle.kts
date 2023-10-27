c6722a93d7770f88997ba0e0379db82ef6ae2609
sarah svitlik <svitliksarah@gmail.com>
2023-10-27 12:18:17 -0400

    No errors in my code, but the tab view is not showing up
    I am going to email the professor and ask him to check out my code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,7 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("androidx.viewpager2:viewpager2:1.0.0")
-    implementation ("me.relex:circleindicator:2.1.6")
+    implementation ("androidx.navigation:navigation-fragment:2.3.0")
+    implementation ("androidx.navigation:navigation-ui:2.3.0")
 
 }
