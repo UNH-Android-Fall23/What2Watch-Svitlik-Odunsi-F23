d3e0520e1ca4835fb564f2735f7335eb835a7815
sarah svitlik <svitliksarah@gmail.com>
2023-11-03 12:54:35 -0400

    Update to the results adapter

--- app/src/main/java/com/example/what2watch_svitlik_odunsi_f23/ui/quizresults/ResultsAdapter.kt
+++ app/src/main/java/com/example/what2watch_svitlik_odunsi_f23/ui/quizresults/ResultsAdapter.kt
@@ -5,7 +5,5 @@ import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.example.what2watch_svitlik_odunsi_f23.R
 
-class ResultsAdapter (private val dataSet: Array<String>) :
-
-
-    }
+class ResultsAdapter (private val dataSet: Array<String>)
+//need to finish later - this is going to connect to a recyler view on the results page
