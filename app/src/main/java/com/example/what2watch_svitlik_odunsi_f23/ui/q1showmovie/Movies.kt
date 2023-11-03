cd4c40c3e38223ad624b58b9c562fcb67323e6de
sarah svitlik <svitliksarah@gmail.com>
2023-11-02 20:09:53 -0400

    Added values to the movies & shows class

--- app/src/main/java/com/example/what2watch_svitlik_odunsi_f23/ui/q1showmovie/Movies.kt
+++ app/src/main/java/com/example/what2watch_svitlik_odunsi_f23/ui/q1showmovie/Movies.kt
@@ -1,4 +1,4 @@
 package com.example.what2watch_svitlik_odunsi_f23.ui.q1showmovie
 
-class Movies {
-}
+class Movies (val title: String, val genre: String, val decade: Long, val reviewRating: String)
+
--- app/src/main/java/com/example/what2watch_svitlik_odunsi_f23/ui/q1showmovie/Shows.kt
+++ app/src/main/java/com/example/what2watch_svitlik_odunsi_f23/ui/q1showmovie/Shows.kt
@@ -1,4 +1,3 @@
 package com.example.what2watch_svitlik_odunsi_f23.ui.q1showmovie
 
-class Shows {
-}
+class Shows (val title: String, val genre: String, val decade: Long, val reviewRating: String)  
