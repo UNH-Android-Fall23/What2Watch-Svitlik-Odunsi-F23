7aadfd356a7432256750c0e662f3d212275f454a
sarah svitlik <svitliksarah@gmail.com>
2023-11-02 20:41:14 -0400

    Started working on the accounts page

--- app/src/main/java/com/example/what2watch_svitlik_odunsi_f23/ui/q1showmovie/Shows.kt
+++ app/src/main/java/com/example/what2watch_svitlik_odunsi_f23/ui/q1showmovie/Shows.kt
@@ -1,3 +1,3 @@
 package com.example.what2watch_svitlik_odunsi_f23.ui.q1showmovie
 
-class Shows (val title: String, val genre: String, val decade: Long, val reviewRating: String)  
+class Shows (val title: String, val genre: String, val decade: Long, val reviewRating: String)
