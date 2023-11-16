b905f124446fd8943a3c44ac6aa04c9a10de869f
Ololade Odunsi <ololade.odunsi@gmail.com>
2023-11-15 20:18:39 -0500

    Splash Screen

--- app/src/main/java/com/example/what2watch_svitlik_odunsi_f23/MainActivity.kt
+++ app/src/main/java/com/example/what2watch_svitlik_odunsi_f23/MainActivity.kt
@@ -1,14 +1,13 @@
 package com.example.what2watch_svitlik_odunsi_f23
 
 import android.os.Bundle
-import android.util.Log
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.what2watch_svitlik_odunsi_f23.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
---
+++ app/src/main/java/com/example/what2watch_svitlik_odunsi_f23/ui/splashScreen/SplashScreenFragment.kt
@@ -0,0 +1,5 @@
+package com.example.what2watch_svitlik_odunsi_f23.ui.splashScreen
+
+
+class SplashScreenFragment {
+}
