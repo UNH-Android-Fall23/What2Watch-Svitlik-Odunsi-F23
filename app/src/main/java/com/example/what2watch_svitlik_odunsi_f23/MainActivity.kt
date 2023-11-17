9428ffa615dcea79c1c01ffdb20fb085b91d54e6
sarah svitlik <sarahtheawesome03@gmail.com>
2023-11-17 13:05:45 -0500

    Fixed constraints on login

--- app/src/main/java/com/example/what2watch_svitlik_odunsi_f23/MainActivity.kt
+++ app/src/main/java/com/example/what2watch_svitlik_odunsi_f23/MainActivity.kt
@@ -2,11 +2,13 @@ package com.example.what2watch_svitlik_odunsi_f23
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.commit
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.what2watch_svitlik_odunsi_f23.databinding.ActivityMainBinding
+import com.example.what2watch_svitlik_odunsi_f23.ui.login.LogInFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
@@ -25,16 +27,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val navView: BottomNavigationView = binding.navView
-
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_browse, R.id.navigation_shuffle, R.id.navigation_profile,
+            // Passing each menu ID as a set of Ids because each
+            // menu should be considered as top level destinations.
+            val appBarConfiguration = AppBarConfiguration(
+                setOf(
+                    R.id.navigation_home,
+                    R.id.navigation_browse,
+                    R.id.navigation_shuffle,
+                    R.id.navigation_profile,
+                )
             )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+            setupActionBarWithNavController(navController, appBarConfiguration)
+            navView.setupWithNavController(navController)
+
+        }
     }
-}
