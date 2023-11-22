5846fb75961dc08cb13bbf4bae04335f52d43877
sarah svitlik <svitliksarah@gmail.com>
2023-11-22 12:32:38 -0500

    Added functions for push notifications
    Nothing has been tested yet, I just put the code in from the android documentation

--- app/src/main/java/com/example/what2watch_svitlik_odunsi_f23/ui/notifications/PushNotifications.kt
+++ app/src/main/java/com/example/what2watch_svitlik_odunsi_f23/ui/notifications/PushNotifications.kt
@@ -1,4 +1,16 @@
 package com.example.what2watch_svitlik_odunsi_f23.ui.notifications
+import android.annotation.SuppressLint
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.example.what2watch_svitlik_odunsi_f23.MainActivity
+import com.example.what2watch_svitlik_odunsi_f23.R
+import com.example.what2watch_svitlik_odunsi_f23.ui.home.HomeFragment
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
@@ -13,8 +25,42 @@ public class PushNotifications : FirebaseMessagingService() {
         }
     }
 
+    @SuppressLint("MissingPermission")
     private fun showNotification(title: String?, body: String?) {
-        // Implement your notification display logic here
-        // This can be similar to the showNotification function in the previous response
+        val channelId = "What2Watch"
+        createNotificationChannel(channelId)
+
+        val intent = Intent(this, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+
+        val builder = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.cinema)
+            .setContentTitle(title)
+            .setContentText(body)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+
+        val notificationManager = NotificationManagerCompat.from(this)
+        val notificationId = 1
+        notificationManager.notify(notificationId, builder.build())
     }
+
+    private fun createNotificationChannel(channelId: String) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "What2Watch"
+            val descriptionText = "Goes to quiz start page"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(channelId, name, importance).apply {
+                description = descriptionText
+            }
+
+            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+
 }
