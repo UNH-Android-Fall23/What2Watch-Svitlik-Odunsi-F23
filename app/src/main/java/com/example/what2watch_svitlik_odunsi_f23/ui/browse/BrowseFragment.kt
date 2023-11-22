fa59e8823abec2f4723121e7af67fcdfea9919ab
sarah svitlik <svitliksarah@gmail.com>
2023-11-22 00:37:22 -0500

    Added a filter on the browse page in XML
    Later the user will click it and be able to filter what genres they want to browse from

--- app/src/main/java/com/example/what2watch_svitlik_odunsi_f23/ui/browse/BrowseFragment.kt
+++ app/src/main/java/com/example/what2watch_svitlik_odunsi_f23/ui/browse/BrowseFragment.kt
@@ -25,6 +25,14 @@ class BrowseFragment : Fragment() {
         val browseViewModel =
             ViewModelProvider(this).get(BrowseViewModel::class.java)
 
+        val btnFilterGenre = binding.btnHamburgerIcon
+
+        btnFilterGenre.setOnClickListener {
+            //When pressed, what shows up ?
+            // Need to put a pop up that would show the different genres you can pick from
+            // Then need to figure out how to change the recycler view based on that filter alone
+        }
+
         _binding = FragmentBrowseBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
