86f2acb5769766212349aa9bff4d269f3800e9b8
Ololade Odunsi <ololade.odunsi@gmail.com>
2023-11-24 16:26:59 -0500

    Revert "Changes to be deleted"
    This reverts commit ff64cd675741eeb6a83a6af25befc4a92bf967e3.

--- app/src/main/java/com/example/what2watch_svitlik_odunsi_f23/ui/choose/FragmentChooseViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.example.what2watch_svitlik_odunsi_f23.ui.choose
-
-import androidx.lifecycle.ViewModel
-
-class FragmentChooseViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
