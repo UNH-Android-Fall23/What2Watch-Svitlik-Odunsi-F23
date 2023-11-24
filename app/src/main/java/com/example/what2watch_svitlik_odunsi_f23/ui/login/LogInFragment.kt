eaf7d77651da7c8780689f0cc96cb49215086e2e
Ololade Odunsi <ololade.odunsi@gmail.com>
2023-11-24 17:52:25 -0500

    Revert "Removing and adding codes"
    This reverts commit c3bdd3453fba7e2a5ba27cd5b550191799c2b1c0.

--- app/src/main/java/com/example/what2watch_svitlik_odunsi_f23/ui/login/LogInFragment.kt
+++ app/src/main/java/com/example/what2watch_svitlik_odunsi_f23/ui/login/LogInFragment.kt
@@ -1,24 +1,24 @@
 package com.example.what2watch_svitlik_odunsi_f23.ui.login
 
 import android.content.ContentValues.TAG
-import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.what2watch_svitlik_odunsi_f23.R
-import com.example.what2watch_svitlik_odunsi_f23.databinding.ActivityLoginBinding
+import com.example.what2watch_svitlik_odunsi_f23.databinding.FragmentLoginBinding
+import androidx.lifecycle.ViewModelProvider
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
-class login : AppCompatActivity() {
+class LogInFragment : Fragment() {
 
     private val db = Firebase.firestore
 
-    private lateinit var _binding: ActivityLoginBinding? = null
+    private var _binding: FragmentLoginBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -30,7 +30,7 @@ class login : AppCompatActivity() {
         savedInstanceState: Bundle?
     ): View {
 
-        _binding = ActivityLoginBinding.inflate(inflater, container, false)
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
         val root : View = binding.root
 
         val btnLogIn = binding.btnLogin
@@ -39,7 +39,6 @@ class login : AppCompatActivity() {
         binding.btnLogin.setOnClickListener {
             Log.d(TAG, "LogIn button was pressed!")
             findNavController().navigate(R.id.navigation_home)
-            startActivity(Intent)
         }
 
         return root
@@ -51,3 +50,6 @@ class login : AppCompatActivity() {
         _binding = null
     }
 }
+
+
+
