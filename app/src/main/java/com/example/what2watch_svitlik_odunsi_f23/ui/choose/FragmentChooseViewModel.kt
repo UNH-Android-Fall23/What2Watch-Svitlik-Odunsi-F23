ff64cd675741eeb6a83a6af25befc4a92bf967e3
Ololade Odunsi <ololade.odunsi@gmail.com>
2023-11-24 16:26:30 -0500

    Changes to be deleted
    Updates to be deleted.

---
+++ app/src/main/java/com/example/what2watch_svitlik_odunsi_f23/ui/choose/FragmentChooseViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.what2watch_svitlik_odunsi_f23.ui.choose
+
+import androidx.lifecycle.ViewModel
+
+class FragmentChooseViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/example/what2watch_svitlik_odunsi_f23/ui/choose/fragment_choose.kt
@@ -0,0 +1,48 @@
+package com.example.what2watch_svitlik_odunsi_f23.ui.choose
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.what2watch_svitlik_odunsi_f23.R
+import com.example.what2watch_svitlik_odunsi_f23.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    val TAG = "SvilikAppHelp"
+
+    private var db = Firebase.firestore
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home,
+                R.id.navigation_browse,
+                R.id.navigation_shuffle,
+                R.id.navigation_profile,
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+}
+
+
