a75c51071c6836503a29d3ea14702cb47c397763
Ololade Odunsi <ololade.odunsi@gmail.com>
2023-11-26 19:15:30 -0500

    Drop-down functionality
    Trying to make the gender and country boxes a dropdown option.

--- app/src/main/java/com/example/what2watch_svitlik_odunsi_f23/Register.kt
+++ app/src/main/java/com/example/what2watch_svitlik_odunsi_f23/Register.kt
@@ -2,6 +2,7 @@ package com.example.what2watch_svitlik_odunsi_f23
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
@@ -26,11 +27,63 @@ class Register : AppCompatActivity() {
         createUserButton.setOnClickListener {
             createUser()
         }
+
+        button.setOnClickListener {
+            val selectedCountry = spinnerCountry.selectedItem.toString()
+            val selectedGender = spinnerGender.selectedItem.toString()
+
+            // ... code to register the user with Firebase ...
+        }
+
+        val database = FirebaseDatabase.getInstance("https://your-firebase-database-url.firebaseio.com/")
+        val myRef = database.getReference("users")
+
+        data class User(
+            val username: String,
+            val email: String,
+            val password: String,
+            val country: String,
+            val gender: String
+        )
+
+        button.setOnClickListener {
+            val selectedCountry = spinnerCountry.selectedItem.toString()
+            val selectedGender = spinnerGender.selectedItem.toString()
+
+            val user = User(
+                editUsername.text.toString(),
+                editEmailAddress.text.toString(),
+                editPassword.text.toString(),
+                selectedCountry,
+                selectedGender
+            )
+
+            myRef.child(user.username).setValue(user)
+                .addOnSuccessListener {
+                    Toast.makeText(this, "User added successfully!", Toast.LENGTH_SHORT).show()
+                }
+                .addOnFailureListener {
+                    Toast.makeText(this, "Failed to add user!", Toast.LENGTH_SHORT).show()
+                }
+        }
+
     }
 
     private fun createUser() {
         val email = findViewById<EditText>(R.id.editEmailAddress).text.toString()
         val password = findViewById<EditText>(R.id.editPassword).text.toString()
+        val countries = arrayOf("USA", "UK", "India", "Canada", "Australia")
+        val genders = arrayOf("Male", "Female", "Other")
+
+        val countryAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, countries)
+        countryAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        val spinnerCountry = null
+        spinnerCountry.adapter = countryAdapter
+
+        val genderAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, genders)
+        genderAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        val spinnerGender = null
+        spinnerGender.adapter = genderAdapter
 
         if (email.isNotEmpty() && password.isNotEmpty()) {
             auth.createUserWithEmailAndPassword(email, password)
