94726fe63049c7c34c94b1c2f88e8ad26c50c797
sarah svitlik <sarahtheawesome03@gmail.com>
2023-11-27 12:32:47 -0500

    Finished Browse recycler list
    Just need to get the actual filtered data in

--- app/src/main/java/com/example/what2watch_svitlik_odunsi_f23/ui/browse/BrowseFragment.kt
+++ app/src/main/java/com/example/what2watch_svitlik_odunsi_f23/ui/browse/BrowseFragment.kt
@@ -7,11 +7,17 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.what2watch_svitlik_odunsi_f23.databinding.FragmentBrowseBinding
+import com.example.what2watch_svitlik_odunsi_f23.ui.quizresults.MoviesAndShowsList
+import com.example.what2watch_svitlik_odunsi_f23.ui.quizresults.RecyclerResultsCard
+import com.example.what2watch_svitlik_odunsi_f23.ui.shuffle.ShuffleRecyclerAdapter
 
 class BrowseFragment : Fragment() {
 
     private var _binding: FragmentBrowseBinding? = null
+    private lateinit var mRecyclerView: RecyclerView
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -33,6 +39,24 @@ class BrowseFragment : Fragment() {
             // Need to put a pop up that would show the different genres you can pick from
             // Then need to figure out how to change the recycler view based on that filter alone
       //  }
+
+        val browseRecyclerList: ArrayList<RecyclerResultsCard> = ArrayList()
+        for (result in MoviesAndShowsList) {
+            browseRecyclerList.add(
+                RecyclerResultsCard(
+                    result.tconst,
+                    result.primaryTitle,
+                    result.titleType,
+                    result.startYear,
+                    result.genre,
+                    result.averageRating)
+            )
+        }
+
+        mRecyclerView = binding.recyclerViewMoviesShows
+        mRecyclerView.setHasFixedSize(true)
+        mRecyclerView.layoutManager = LinearLayoutManager(context)
+        mRecyclerView.adapter = BrowseRecyclerAdapter(browseRecyclerList, this)
 
         _binding = FragmentBrowseBinding.inflate(inflater, container, false)
         val root: View = binding.root
---
+++ app/src/main/java/com/example/what2watch_svitlik_odunsi_f23/ui/browse/BrowseRecyclerAdapter.kt
@@ -0,0 +1,57 @@
+package com.example.what2watch_svitlik_odunsi_f23.ui.browse
+
+import com.example.what2watch_svitlik_odunsi_f23.ui.quizresults.RecyclerResultsCard
+import android.content.ContentValues.TAG
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.what2watch_svitlik_odunsi_f23.R
+
+
+class BrowseRecyclerAdapter (
+    private val mExampleList: ArrayList<RecyclerResultsCard>, // takes in a list
+    private val context: BrowseFragment //pass in the context of results fragment to link data from one to another
+) : RecyclerView.Adapter<BrowseRecyclerAdapter.ExampleViewHolder>() {
+    override fun onCreateViewHolder( //where you generate view for recycler view
+        parent: ViewGroup,
+        viewType: Int
+    ): ExampleViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.movies_shows_card, parent, false)
+        return ExampleViewHolder(itemView)
+    }
+
+    override fun getItemCount(): Int {
+        return mExampleList.size
+    }
+
+    override fun onBindViewHolder(holder: ExampleViewHolder, position: Int) {
+        val (imageResource, name, type, genre, year, rating) = mExampleList[position]
+        //  holder.mImageView.setImageResource(imageResource)
+        holder.mTextView1.text = name
+        holder.mTextView2.text = type
+        holder.mTextGenre.text = genre.toString()
+        holder.mTextYear.text = year.toString()
+        holder.mTextRating.text = rating.toString()
+
+
+        holder.itemView.setOnClickListener {
+            Log.d(TAG, "Clicked position i s$position")
+        }
+    }
+
+    class ExampleViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val mImageView: ImageView = itemView.findViewById(R.id.image_view)
+        val mTextView1: TextView = itemView.findViewById(R.id.text_name)
+        val mTextView2: TextView = itemView.findViewById(R.id.text_type1)
+        val mTextGenre: TextView = itemView.findViewById(R.id.text_genre)
+        val mTextYear: TextView = itemView.findViewById(R.id.text_year)
+        val mTextRating: TextView = itemView.findViewById(R.id.text_rating)
+    }
+
+}
+
+
