9cec887c5746df93a1e66cf088c777bbfef13dc7
Ololade Odunsi <ololade.odunsi@gmail.com>
2023-11-28 15:05:19 -0500

    Trying to make the register page work.

--- app/src/main/java/com/example/what2watch_svitlik_odunsi_f23/Register.kt
+++ app/src/main/java/com/example/what2watch_svitlik_odunsi_f23/Register.kt
@@ -1,121 +1,42 @@
 package com.example.what2watch_svitlik_odunsi_f23
-import android.annotation.SuppressLint
 import android.os.Bundle
-import android.util.Log
-import android.widget.ArrayAdapter
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.database.FirebaseDatabase
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 
-class Register : AppCompatActivity() {
-    private lateinit var auth: FirebaseAuth
-    private lateinit var database: FirebaseDatabase
+class RegisterFragment : Fragment() {
 
-    @SuppressLint("MissingInflatedId")
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.activity_register, container, false)
 
-        auth = FirebaseAuth.getInstance()
-        database = FirebaseDatabase.getInstance()
+        //initialize EditText and Button
+        val editEmailAddress = view.findViewById<EditText>(R.id.editEmailAddress)
+        val editPassword = view.findViewById<EditText>(R.id.editPassword)
+        val registerButton = view.findViewById<Button>(R.id.Registerbutton)
 
-        val createUserButton = findViewById<Button>(R.id.Registerbutton)
-        createUserButton.setOnClickListener {
-            createUser()
+        //handle register button click
+        registerButton.setOnClickListener {
+            //get the user's email and password
+            val email = editEmailAddress.text.toString()
+            val password = editPassword.text.toString()
+
+            //validate the email and password
+            if (email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(activity, "Please enter your email and password", Toast.LENGTH_SHORT).show()
+            } else {
+                //if the user's input is valid, navigate to fragment_home
+                findNavController().navigate(R.id.action_nav_host_fragment_to_navigation_home)
+            }
         }
 
-        button.setOnClickListener {
-            val selectedCountry = spinnerCountry.selectedItem.toString()
-            val selectedGender = spinnerGender.selectedItem.toString()
-
-            // ... code to register the user with Firebase ...
-        }
-
-        val database = FirebaseDatabase.getInstance("https://your-firebase-database-url.firebaseio.com/")
-        val myRef = database.getReference("users")
-
-        data class User(
-            val username: String,
-            val email: String,
-            val password: String,
-            val country: String,
-            val gender: String
-        )
-
-        button.setOnClickListener {
-            val selectedCountry = spinnerCountry.selectedItem.toString()
-            val selectedGender = spinnerGender.selectedItem.toString()
-
-            val user = User(
-                editUsername.text.toString(),
-                editEmailAddress.text.toString(),
-                editPassword.text.toString(),
-                selectedCountry,
-                selectedGender
-            )
-
-            myRef.child(user.username).setValue(user)
-                .addOnSuccessListener {
-                    Toast.makeText(this, "User added successfully!", Toast.LENGTH_SHORT).show()
-                }
-                .addOnFailureListener {
-                    Toast.makeText(this, "Failed to add user!", Toast.LENGTH_SHORT).show()
-                }
-        }
-
-    }
-
-    private fun createUser() {
-        val email = findViewById<EditText>(R.id.editEmailAddress).text.toString()
-        val password = findViewById<EditText>(R.id.editPassword).text.toString()
-        val countries = arrayOf("USA", "UK", "India", "Canada", "Australia")
-        val genders = arrayOf("Male", "Female", "Other")
-
-        val countryAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, countries)
-        countryAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        val spinnerCountry = null
-        spinnerCountry.adapter = countryAdapter
-
-        val genderAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, genders)
-        genderAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        val spinnerGender = null
-        spinnerGender.adapter = genderAdapter
-
-        if (email.isNotEmpty() && password.isNotEmpty()) {
-            auth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener { task ->
-                    if (task.isSuccessful) {
-                        Log.d(TAG, "createUserWithEmail:success")
-                        val user = auth.currentUser
-                        updateUI(user)
-                    } else {
-                        Log.w(TAG, "createUserWithEmail:failure", task.exception)
-                        Toast.makeText(baseContext, "Authentication failed.",
-                            Toast.LENGTH_SHORT).show()
-                        updateUI(null)
-                    }
-                }
-        } else {
-            Toast.makeText(baseContext, "Please enter your email and password.",
-                Toast.LENGTH_SHORT).show()
-        }
-    }
-
-    private fun updateUI(user: FirebaseUser?) {
-        if (user != null) {
-            val uid = user.uid
-            database.getReference("users/$uid").setValue(user.displayName)
-            // ...
-        } else {
-            // ...
-        }
-    }
-
-    companion object {
-        private const val TAG = "MainActivity"
+        return view
     }
 }
