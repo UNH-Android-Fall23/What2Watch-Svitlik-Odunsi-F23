e5c8ee2390b7688bf3ca4d67f93a5240d397fa05
Ololade Odunsi <ololade.odunsi@gmail.com>
2023-11-30 19:21:11 -0500

    User's profile Activity created.
    This is where the user can go see their profile information and edit them.

---
+++ app/src/main/java/com/example/what2watch_svitlik_odunsi_f23/userProfile.kt
@@ -0,0 +1,11 @@
+package com.example.what2watch_svitlik_odunsi_f23
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class userProfile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_profile)
+    }
+}
