1ab601f9e3fb0dac30bab0bacdbb38d4ba7c500e
sarah svitlik <sarahtheawesome03@gmail.com>
2023-12-08 19:16:47 -0500

    Got it so the register page starts up upon loading

--- app/src/main/java/com/example/what2watch_svitlik_odunsi_f23/RegisterActivity.kt
+++ app/src/main/java/com/example/what2watch_svitlik_odunsi_f23/RegisterActivity.kt
@@ -8,8 +8,6 @@ import android.widget.EditText
 import android.widget.Spinner
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.example.what2watch_svitlik_odunsi_f23.R
-import com.example.what2watch_svitlik_odunsi_f23.R.id.genderSpinner
 import com.example.what2watch_svitlik_odunsi_f23.ui.home.HomeFragment
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
@@ -104,7 +102,7 @@ class RegisterActivity : AppCompatActivity() {
     private fun updateUI(user: FirebaseUser?) {
         if (user != null) {
             // navigate to the home fragment
-           // val intent = Intent(this, HomeActivity::class.java)
+            val intent = Intent(this, HomeFragment::class.java)
             startActivity(intent)
         } else {
             // Prompt the user to try again with different credentials
